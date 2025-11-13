a5f5a3143cacd438fa6f89cc243932522935d2af
Yukihisa-Hida <h2511198@gl.cc.uec.ac.jp>
2025-11-14 04:48:22 +0900

    結果終わり

---
+++ graph/R2.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+# import matplotlib_fontja
+# import pandas as pd
+import numpy as np
+import math
+
+plt.rcParams["font.family"] = "MS Mincho"
+plt.rcParams["font.size"] = 15
+
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+
+plt.rcParams["legend.fancybox"] = False
+plt.rcParams["legend.framealpha"] = 1
+plt.rcParams["legend.edgecolor"] = "black"
+plt.rcParams["legend.markerscale"] = 5
+plt.rcParams["lines.linewidth"] = 1
+plt.rcParams["lines.linestyle"] = ""
+plt.rcParams["lines.marker"] = "."
+
+fig = plt.figure(dpi=200)
+
+x1 = 0
+x2 = 250
+y1 = -1.5
+y2 = 0
+
+plt.xlim(x1, x2)
+plt.ylim(y1, y2)
+
+x = np.array([0, 53, 106, 159, 212])
+y = np.array([0, -0.295121136, -0.584569259, -0.86332286, -1.164352856])
+
+plt.plot(x, y, markeredgecolor='black', marker='o', markersize=5, color='black')
+
+plt.rcParams["lines.linestyle"] = "-"
+
+slope, intercept = np.polyfit(x, y, 1)
+xmin, xmax = plt.xlim()
+x_fit = np.array([xmin, xmax])
+y_fit = slope * x_fit + intercept
+plt.plot(x_fit, y_fit, marker='', color='black')
+print(f'最小二乗法による直線 (LSM Line)\ny = {slope:.6f}x + {intercept:.6f}')
+plt.text(125, -1.4, f'y = {slope:.6f}x + {intercept:.6f}', fontsize=12)
+
+plt.xlabel(r"時間$t_i\ \mathrm{\mu s}$")
+plt.ylabel(r"電圧比の対数$\ \log{V_i/V_1}$")
+
+plt.tight_layout()
+
+plt.show()
